bb98d479208bb2f95a67f8e2d12b0cd9637e2049
0xdeadbad <matheus.dasilva.garcias@gmail.com>
2025-05-25 22:55:44 +0000

    preparing for new branch

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	_ "time"
 
 	_ "github.com/0xdeadbad/venhaparaoleds-devops/docs"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -17,6 +20,14 @@ import (
 func appGoroutine(ctx context.Context, cancel context.CancelCauseFunc, app *fiber.App) {
 	err := app.Listen(":3000")
 	cancel(err)
+}
+
+// Load .env as environment variables if possible
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file couldn't be loaded.")
+	}
 }
 
 // @title Fiber Example API
@@ -29,59 +40,12 @@ func appGoroutine(ctx context.Context, cancel context.CancelCauseFunc, app *fibe
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @host localhost:8080
 // @BasePath /
-// func main() {
-// 	app := fiber.New()
-
-// 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-// 	defer cancel()
-
-// 	app.Get("/swagger/*", swagger.HandlerDefault) // default
-
-// 	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-// 		URL:         "http://example.com/doc.json",
-// 		DeepLinking: false,
-// 		// Expand ("list") or Collapse ("none") tag groups by default
-// 		DocExpansion: "none",
-// 		// Prefill OAuth ClientId on Authorize popup
-// 		OAuth: &swagger.OAuthConfig{
-// 			AppName:  "OAuth Provider",
-// 			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
-// 		},
-// 		// Ability to change OAuth2 redirect uri location
-// 		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
-// 	}))
-
-// 	done := make(chan any, 1)
-// 	go func() {
-// 		if err := app.Listen(":3000"); err != nil {
-// 			fmt.Printf("%+v\n", err)
-// 		}
-// 		done <- struct{}{}
-// 	}()
-
-// main_loop:
-//
-//		for {
-//			select {
-//			case <-ctx.Done():
-//				if err := app.Shutdown(); err != nil {
-//					fmt.Printf("%+v\n", err)
-//				}
-//				break
-//			case <-done:
-//				break main_loop
-//			}
-//		}
-//	}
-
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(".env file couldn't be loaded.")
-	}
-}
-
 func main() {
+	app := fiber.New()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
 	POSTGRES_HOSTNAME := os.Getenv("POSTGRES_HOSTNAME")
 	POSTGRES_USER := os.Getenv("POSTGRES_USER")
 	POSTGRES_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
@@ -96,4 +60,28 @@ func main() {
 	}
 
 	db.AutoMigrate(&Applicant{})
+
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+
+	done := make(chan any, 1)
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			fmt.Printf("%+v\n", err)
+		}
+		done <- struct{}{}
+	}()
+
+main_loop:
+
+	for {
+		select {
+		case <-ctx.Done():
+			if err := app.Shutdown(); err != nil {
+				fmt.Printf("%+v\n", err)
+			}
+			break
+		case <-done:
+			break main_loop
+		}
+	}
 }
